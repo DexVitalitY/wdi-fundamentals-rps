7fc95b10aff3421d72115b22d097ebff93ed434c
Michael Chow <DexVitalitY@Michaels-MacBook-Pro.local>
2015-03-11 23:36:49 +0800

    Rock Paper Scissors Project Submit

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -1,10 +1,9 @@
 ////////////////////////////////////////////////
 /*   Provided Code - Please Don't Edit   */
 ////////////////////////////////////////////////
-'use strict';
 
 function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
+    console.log(" Please choose either 'rock', 'paper', or 'scissors'. \n")
     return prompt();
 }
 function randomPlay() {
@@ -22,45 +21,46 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-	var move = getInput();
-	return move;
+	playerMove = getInput();
+	return playerMove;
 }
 
 function getComputerMove(move) {
-	var move = randomPlay();
-	return move;
+	computerMove = randomPlay();
+	return computerMove;
 }
 
 function getWinner(playerMove,computerMove) {
     var winner;
-    if (playerMove === rock && computerMove === rock) {
+    if (playerMove == "rock" && computerMove == "rock") {
     	winner = "tie";
     	return winner;
-    } else if (playerMove === rock && computerMove === paper) {
-    	winner = "computer";
+    } else if (playerMove == "rock" && computerMove == "paper") {
+    	winner = "BORG";
     	return winner;
-    } else if (playerMove === rock && computerMove === scissors){
+    } else if (playerMove == "rock" && computerMove == "scissors"){
     	winner = "player";
     	return winner;
-    } else if (playerMove === paper && computerMove === paper) {
+    } else if (playerMove == "paper" && computerMove == "paper") {
     	winner = "tie";
     	return winner;
-    } else if (playerMove === paper && computerMove === scissors) {
-    	winner = "computer";
+    } else if (playerMove == "paper" && computerMove == "scissors") {
+    	winner = "BORG";
     	return winner;
-    } else if (playerMove === paper && computerMove === rock) {
+    } else if (playerMove == "paper" && computerMove == "rock") {
     	winner = "player";
     	return winner;
-    } else if (playerMove === scissors && computerMove === paper) {
-    	winner ="player"
+    } else if (playerMove == "scissors" && computerMove == "paper") {
+    	winner = "player"
     	return winner;
-    } else if (playerMove === scissors && computerMove === scissors) {
+    } else if (playerMove == "scissors" && computerMove == "scissors") {
     	winner = "tie";
     	return winner; 
-    } else if (playerMove === scissors && computerMove === rock) { 
-    	winner = "computer";
+    } else if (playerMove == "scissors" && computerMove == "rock") { 
+    	winner = "BORG";
     	return winner;
     }
+    console.log(winner);
 }
 
 function playToFive() {
@@ -69,7 +69,47 @@ function playToFive() {
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
-    return [playerWins, computerWins];
-}
+    while (playerWins < 5 || computerWins < 5) {
+        
+  getPlayerMove();
+  getComputerMove();
+  getWinner(playerMove, computerMove);
+  
+    if (getWinner(playerMove, computerMove) == "BORG") {
+    	computerWins += 1;
+    	if (computerWins == 5) {
+    	    return [ "BORG WINS! RESISTANCE IS FUTILE! YOU WILL BE ASSIMILATED! Player Score: " + playerWins, "Computer Score: " + computerWins]
+    	} 
+        } else if (getWinner(playerMove, computerMove) == "player") {
+    	playerWins += 1;
+    	if (playerWins == 5) {
+    	    return [ "PLAYER WINS! THE BORG Have been Defeated! Player Score: " + playerWins, "Computer Score: " + computerWins];
+    	} 
+	} 
+  console.log("Player chose " + playerMove + " while BORG chose " + computerMove + "\n");
+  console.log("The score is currently Player: " + playerWins + " to  Computer: " + computerWins + "\n");        } 
+    }
+
+
+
+
+return [playerWins, computerWins];
+
+
+
+	if (playerMove != "rock" || "paper" || "scissors") {
+	    console.log("Please make sure to input rock, paper, or scissors in order to play! \n");
+	} 
+
+
+
+
+
+
+
+
+
+
+
 
 
